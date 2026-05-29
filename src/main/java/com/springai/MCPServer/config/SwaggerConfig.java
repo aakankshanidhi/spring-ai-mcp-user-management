a5d4f001c6acac1df7043811cd7b2134822eac60
Aakanksha Nidhi <akankshanidhi0211@email.com>
2026-05-29 17:06:12 +0530

    Add Swagger JWT authentication

--- src/main/java/com/springai/MCPServer/config/SwaggerConfig.java
+++ src/main/java/com/springai/MCPServer/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.springai.MCPServer.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
 
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
 
                 .info(new Info()
@@ -25,6 +30,24 @@ public class SwaggerConfig {
                 .servers(List.of(
                         new Server()
                                 .url("https://spring-ai-mcp-user-management-production.up.railway.app")
-                ));
+                ))
+
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(securitySchemeName)
+                )
+
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        securitySchemeName,
+
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
     }
 }
